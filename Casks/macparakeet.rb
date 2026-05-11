0dc531741545fdaeaa57691cf6ce57363b900f0b
Dick Tang <dick.tang@ymail.com>
2026-05-11 17:44:36 +0800

    add macparakeet

---
+++ Casks/macparakeet.rb
@@ -0,0 +1,24 @@
+cask "macparakeet" do
+  version "0.6.4"
+  sha256 "02820e86755c6f2ccdf0a2ee7f234520a25113ae27db48d3c667a96a27919cd8"
+
+  url "https://github.com/moona3k/macparakeet/releases/download/v#{version}/MacParakeet.dmg"
+  name "MacParakeet"
+  desc "Fast voice app for Mac with fully local speech and optional AI"
+  homepage "https://macparakeet.com"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "MacParakeet.app"
+
+  zap trash: [
+    "~/Library/Application Support/MacParakeet",
+    "~/Library/Preferences/com.macparakeet.plist",
+  ]
+end
